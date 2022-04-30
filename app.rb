e063662fb74dd3e694824862ff0f05af0f246d8b
khanhnguyenvinh2002 <giakhanhvy@gmail.com>
2022-04-30 04:41:24 -0400

    update

--- app.rb
+++ app.rb
@@ -1,10 +1,14 @@
 require 'sinatra'
+# require 'sinatra/activerecord'
 require 'bunny'
 require 'json'
-Dir[File.dirname(__FILE__) + '/service/*.rb'].each { |file| require file }
+require "redis"
+
+Dir[File.dirname(__FILE__) + '/config/initializers/*.rb'].each { |file| require file }
 Dir[File.dirname(__FILE__) + '/api/*.rb'].each { |file| require file }
 
 get '/' do
+  require_relative './service/worker.rb'
   erb :index
 end
 
