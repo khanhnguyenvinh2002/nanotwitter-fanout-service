7fd5ca59ae4474eb24a1a5b77e4064c6acc9d970
khanhnguyenvinh2002 <giakhanhvy@gmail.com>
2022-04-30 14:22:52 -0400

    "a"

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ require "redis"
 
 Dir[File.dirname(__FILE__) + '/config/initializers/*.rb'].each { |file| require file }
 Dir[File.dirname(__FILE__) + '/api/*.rb'].each { |file| require file }
-
+require_relative './service/redis.rb'
 get '/' do
   Thread.new do
     require_relative './service/worker.rb'
